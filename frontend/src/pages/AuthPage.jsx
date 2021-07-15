a449fc4cd555e914fcc6fa3df36fbdfe76d4ca47
MikitaCat <nikita.proger21@gmail.com>
2021-07-15 22:43:28 +0300

    Create markup in AuthPage with materializecss

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,9 +1,46 @@
-import React from "react"
+import React from 'react';
 
 export const AuthPage = () => {
-    return(
-        <div>
-            <h1>Auth Page</h1>
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Make Links Beautiful</h1>
+
+        <div className="card red lighten-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authorisation</span>
+            <div>
+              <div className="input-field">
+                <input
+                  placeholder="Enter your email"
+                  className="color-input"
+                  id="email"
+                  type="text"
+                  name="email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Enter your password"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="color-input"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button className="btn red lighten-3" style={{ marginRight: 10 }}>
+              Sing In
+            </button>
+            <button className="btn red lighten-2">Log in</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
