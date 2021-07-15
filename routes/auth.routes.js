1ce8f0d157fec8351649b511377635445a99b330
MikitaCat <nikita.proger21@gmail.com>
2021-07-15 16:52:17 +0300

    Created a check for the existence of a user in auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,17 @@
 const {Router} = require("express")
+const User = require("../models/User")
 const router = Router()
+
 
 router.post('/register', async (req, res) => {
     try {
         const {email, password} = req.body
+
+        const candidate = await User.findOne({email})
+
+        if(candidate) {
+           return res.status(400).json({message: "This user is already exist"})
+        }
     } catch (e) {
         res.status(500).json({message: "Something went wrong. Try again"})
     }
