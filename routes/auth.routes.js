a4d601b823c4a15b718bbb9230db0305abf226fc
MikitaCat <nikita.proger21@gmail.com>
2021-07-15 18:49:17 +0300

    Created login logic with jwt in auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require("express")
 const bcrypt = require("bcryptjs")
+const config = require("../config/default.json")
+const jwt = require("jsonwebtoken")
 const {check, validationResult} = require("express-validator")
 const User = require("../models/User")
 const router = Router()
@@ -29,7 +31,7 @@ router.post(
         //Get data from Front End
         const {email, password} = req.body
 
-        //checking user existense
+        //checking user existense (if user is already exist it can't register again)
         const candidate = await User.findOne({email})
 
         if(candidate) {
@@ -49,8 +51,53 @@ router.post(
     }
 })
 
-router.post('/login', async (req, res) => {
+router.post(
+    '/login',
 
+    //Validators array
+    [
+        check("email", "Enter correct email").normalizeEmail().isEmail(),
+        check("password", "Enter correct password").exists()
+    ],
+ async (req, res) => {
+    try {
+        //Validation data
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Not all the fields are filled correctly"
+            })
+        }
+
+        //Get Data from request body
+        const {email, data} = req.body
+
+        //Checking user existense (if user is not exist, it can't login)
+        const user = User.findOne({email})
+
+        if(!user) {
+            return res.status(400).json({message: "User is undefined"})
+        }
+
+        //Checking passwords matching
+        const isMatch = bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.status(400).json({message: "Incorrect password"})
+        }
+
+        //Token creating
+        const token = jwt.sign(
+            {userId: user.id},   //user id
+            config.get('jwtSecret'),  //secret string
+            {expiresIn: "1h"}  //token lifetime
+        )
+        res.json({token, userId: user.id})
+    } catch (e) {
+        res.status(500).json({message: "Something went wrong. Try again"})
+    }
 })
 
 
